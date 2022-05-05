b561476460de42a8c6f32b88e9da0254bc4c1682
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-05-05 09:53:30 -0300

    adding splash screen and search bar

--- app/src/main/java/br/senai/sp/jandira/senaigameapp/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/senaigameapp/MainActivity.kt
@@ -9,9 +9,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater);
+
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding = ActivityMainBinding.inflate(layoutInflater);
     }
 }
--- app/src/main/java/br/senai/sp/jandira/senaigameapp/SplashActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/senaigameapp/SplashActivity.kt
@@ -1,7 +1,10 @@
 package br.senai.sp.jandira.senaigameapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -9,5 +12,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
